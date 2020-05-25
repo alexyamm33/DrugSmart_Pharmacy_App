af8dca614cc45669eb95d67b5f8b22de43c18e68
Alex Yeh <ayeh0330@gmaill.com>
2020-05-25 15:51:59 +0800

    Initial Commit

---
+++ pharmacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pharmacy
+//
+//  Created by Alex Yeh on 2020-05-25.
+//  Copyright © 2020 Alex Yeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
